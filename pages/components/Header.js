9f47cb3ffa657483a746cd11e240030c66362d51
Barham Farraj <barhamfarraj@icloud.com>
2022-06-08 20:16:12 +0300

    done

---
+++ pages/components/Header.js
@@ -0,0 +1,8 @@
+function Header() {
+  return (
+
+    <div className="bg-[#575D90] h-14  place-items-center"><h1 className="p-2.5 font-bold font-mono text-2xl not-italic hover:italic">Cookie Stand Admin</h1></div>
+
+  )
+}
+export default Header;
---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Main from "./components/Main";
+
+function Home() {
+  return (
+    <>
+      <div className="h-screen w-screen ">
+      <Header/>
+      <Main />
+      <Footer/>
+      </div>
+    </>
+  );
+}
+
+export default Home;
